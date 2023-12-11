dfa219a27189ef7f2d854a61bc2cd843133694d4
Benyapa-15 <150587034+Benyapa-15@users.noreply.github.com>
2023-12-11 23:20:28 +0700

    Update kr.py

--- kr.py
+++ kr.py
@@ -5,34 +5,34 @@ import pandas as pd
 
 # Get the API key from the sidebar called OpenAI API key
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
-#user_api_key = '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'
-#user_api_key = 'YOUR_API_KEY_HERE'  # Replace with your actual API key
-#client = openai.ChatCompletion.create(api_key=user_api_key)
-
-#user_api_key = 'YOUR_API_KEY_HERE'  # Replace with your actual API key
-#client = openai.api_key = user_api_key
 
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """Act as a Korean-English-Thai translator. You will receive a 
-            Korean text and you should give the translation in English and Thai.
-            You will translate the text into English first and then translate the English text into Thai.
+            Korean lyrics and you should give the translation in English and Thai.
+            There would be 3 cases:
+            1. You will translate the Korean text into English.
+            2. You will translate the Korean text into Thai.
+            3. You will translate the Korean text into English first and then translate the English text into Thai.
             If the given text is already in English, you should translate it into Thai directly.
             List the translation in a JSON array, one translation per line.
-            Each translation should have 3 fields:
+            Each translation should have 4 fields:
             - "Korean Original Text" - the Korean original text before the translation
             - "English Translation" - the text translated into English
-            - "Thai Translation" - the text translated into Thai
+            - "Thai Translation from Korean text" - the Korean text directly translated into Thai
+            - "Thai Translation from English text" - the English translation translated into Thai
             For example:
             [
                 {
                     "Korean Original Text": "안녕하세요. 저는 한국어를 배우고 있습니다.",
                     "English Translation": "Hello. I am learning Korean.",
-                    "Thai Translation": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลี"
+                    "Thai Translation from Korean text": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลีอยู่ค่ะ"
+                    "Thai Translation from English text": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลี"
                 },
                 {
                     "Korean Original Text": "안녕하세요. 저는 한국어를 배우고 있습니다.",
                     "English Translation": "Hello. I am learning Korean.",
-                    "Thai Translation": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลี"
+                    "Thai Translation from Korean text": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลีอยู่ค่ะ"
+                    "Thai Translation from English text": "สวัสดีค่ะ ฉันกำลังเรียนภาษาเกาหลี"
                 }
             ]
         """    
@@ -45,7 +45,7 @@ st.markdown('Input the Korean text that you want to translate. \n\
             Korean lyrics with some English words are also welcome here. \n\
             Enjoy!')
 
-user_input = st.text_area("Enter some text to translate:", "Your text here")
+user_input = st.text_area("Enter some text to correct:", "Your text here")
 
 
 # submit button after text input
